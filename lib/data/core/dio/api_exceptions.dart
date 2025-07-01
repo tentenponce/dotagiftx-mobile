beeac4bc0fcc0452e5d5537498fbed282b163bf6
Tenten Ponce <exequiel.ponce@angkas.com>
2025-07-02 02:32:52 +0800

    Cleanup API exceptions

--- lib/data/core/dio/api_exceptions.dart
+++ lib/data/core/dio/api_exceptions.dart
@@ -1,55 +1,30 @@
 import 'package:dio/dio.dart';
 
 class ApiException extends DioException {
-  final String? errorCode;
-  final String? errorMessage;
-
-  ApiException({required DioException error, this.errorCode, this.errorMessage})
+  ApiException({required DioException error})
     : super(
         requestOptions: error.requestOptions,
         response: error.response,
         error: error.error,
       );
-
-  String get method => requestOptions.method;
-
-  String? get reasonPhrase => response?.statusMessage;
-
-  Object? get responseBody => response?.data;
-
-  int? get statusCode => response?.statusCode;
-
-  Uri get uri => requestOptions.uri;
 }
 
 class BadRequestException extends ApiException {
-  BadRequestException({
-    required super.error,
-    super.errorCode,
-    super.errorMessage,
-  });
+  BadRequestException({required super.error});
 }
 
 class NetworkTimeoutException extends ApiException {
-  NetworkTimeoutException({
-    required super.error,
-    super.errorCode,
-    super.errorMessage,
-  });
+  NetworkTimeoutException({required super.error});
 }
 
 class ServerUnavailableException extends ApiException {
-  ServerUnavailableException({
-    required super.error,
-    super.errorCode,
-    super.errorMessage,
-  });
+  ServerUnavailableException({required super.error});
 }
 
 class UnauthorizedException extends ApiException {
-  UnauthorizedException({required super.error, super.errorCode});
+  UnauthorizedException({required super.error});
 }
 
 class UnknownException extends ApiException {
-  UnknownException({required super.error, super.errorCode, super.errorMessage});
+  UnknownException({required super.error});
 }
--- lib/data/core/dio/dio_error_interceptor.dart
+++ lib/data/core/dio/dio_error_interceptor.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:dotagiftx_mobile/core/utils/string_utils.dart';
 import 'package:dotagiftx_mobile/data/core/dio/api_exceptions.dart';
 
 class DioErrorInterceptor extends Interceptor {
@@ -14,59 +13,22 @@ class DioErrorInterceptor extends Interceptor {
 
   static ApiException _errorToException(DioException error) {
     final statusCode = error.response?.statusCode ?? 0;
-    final errorMap = _tryGetErrorCode(error.response?.data);
-
-    final errorCode =
-        (errorMap?['code'] ?? errorMap?['error_code'])?.toString();
-    final errorMessage =
-        !StringUtils.isNullOrEmpty(errorMap?['message'].toString())
-            ? errorMap!['message'].toString()
-            : error.message;
 
     if (statusCode >= 400 && statusCode < 500) {
       return switch (statusCode) {
-        401 => UnauthorizedException(error: error, errorCode: errorCode),
-        _ => BadRequestException(
-          error: error,
-          errorCode: errorCode,
-          errorMessage: errorMessage,
-        ),
+        401 => UnauthorizedException(error: error),
+        _ => BadRequestException(error: error),
       };
     } else if (statusCode >= 500 && statusCode < 600) {
-      return ServerUnavailableException(
-        error: error,
-        errorCode: errorCode,
-        errorMessage: errorMessage,
-      );
+      return ServerUnavailableException(error: error);
     } else if (error.type == DioExceptionType.connectionTimeout ||
         error.type == DioExceptionType.unknown ||
         error.type == DioExceptionType.receiveTimeout ||
         error.type == DioExceptionType.sendTimeout ||
         error.type == DioExceptionType.connectionError) {
-      return NetworkTimeoutException(
-        error: error,
-        errorCode: errorCode,
-        errorMessage: errorMessage,
-      );
+      return NetworkTimeoutException(error: error);
     } else {
-      return UnknownException(
-        error: error,
-        errorCode: errorCode,
-        errorMessage: errorMessage,
-      );
-    }
-  }
-
-  static Map<String?, Object?>? _tryGetErrorCode(Object? response) {
-    if (response == null || (response is String && response.isEmpty)) {
-      return null;
-    }
-
-    try {
-      return (response as Map?)?.cast<String?, Object?>();
-    } catch (e) {
-      // if failed casting, try casting the data to string
-      return {'message': response.toString()};
+      return UnknownException(error: error);
     }
   }
 }
