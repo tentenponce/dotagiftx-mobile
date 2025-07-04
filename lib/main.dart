623242c7a1c98cf57e733de3ca4cad6a47ff1b40
Tenten Ponce <poncetenten10@gmail.com>
2025-07-05 02:39:04 +0800

    Added log in unhandled errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:dotagiftx_mobile/core/logging/logger.dart';
 import 'package:dotagiftx_mobile/core/platform/app_platform/app_platform.dart';
 import 'package:dotagiftx_mobile/di/dependency_injection.dart';
 import 'package:dotagiftx_mobile/presentation/app/app.dart';
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
 
 void main() {
   runZonedGuarded(() {
@@ -24,7 +24,7 @@ void main() {
     registerInitialDependencies();
     configureDependencies();
 
-    final appPlatform = GetIt.instance<AppPlatform>();
+    final appPlatform = getIt<AppPlatform>();
     unawaited(appPlatform.initPlatform());
 
     runApp(const App());
@@ -32,5 +32,11 @@ void main() {
 }
 
 void catchUnhandledExceptions(Object error, StackTrace? stack) {
-  debugPrintStack(stackTrace: stack, label: error.toString());
+  final logger = getIt<Logger>();
+  logger.log(
+    LogLevel.error,
+    'Unhandled exception from main: $error',
+    error,
+    stack,
+  );
 }
