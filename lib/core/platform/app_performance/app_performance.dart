c992d65759faa262b4acc8f813259cf3a70d65de
Tenten Ponce <poncetenten10@gmail.com>
2025-07-05 02:42:22 +0800

    Cleanup

--- lib/core/platform/app_performance/app_performance.dart
+++ lib/core/platform/app_performance/app_performance.dart
@@ -1,9 +1,6 @@
 abstract interface class AppPerformance {
-  // list of traces
-  static const String appSplashDuration = 'app_splash_duration';
-
+  Future<void> setAttribute(String name, String attributeName, String value);
+  Future<void> setMetric(String name, String metricName, int value);
   Future<void> startTrace(String name);
   Future<void> stopTrace(String name);
-  Future<void> setMetric(String name, String metricName, int value);
-  Future<void> setAttribute(String name, String attributeName, String value);
 }
